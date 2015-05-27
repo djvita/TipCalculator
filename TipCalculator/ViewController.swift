e47823dae9440fa4edc2c470d6887ed0bce4cb8a
Vita V <dj.vita.09@gmail.com>
2015-05-26 20:35:47 -0500

    Done, Run in iOS Simulator.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var tiprateSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
+    @IBAction func calculateTapped(sender: AnyObject) {
+        //1- get the total bill
+
+        var userInput = billTextField.text as NSString
+        var totalBill: Float = userInput.floatValue
+        
+        //2- determine tip rate
+        
+        var index: Int = tiprateSegmentedControl.selectedSegmentIndex
+        var tipRate: Float = 0.15
+        if index == 0 {
+            tipRate = 0.15
+        } else if index == 1 {
+                tipRate = 0.20
+        } else {
+            tipRate = 0.25
+        }
+        
+        //3- calculate tip
+        
+        var tip: Float = totalBill * tipRate
+        
+        //4- display tip
+        
+        tipLabel.text = "$\(tip)"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
